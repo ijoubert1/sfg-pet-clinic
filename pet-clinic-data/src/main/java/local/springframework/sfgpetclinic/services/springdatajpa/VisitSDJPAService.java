daf51b27788d91ee39676dbb738d859931406c3a
irianjoubert1 <irian.joubert@gmail.com>
2020-08-30 12:05:10 -0300

    Implementing VisitSDJPAService. Closes #47

---
+++ pet-clinic-data/src/main/java/local/springframework/sfgpetclinic/services/springdatajpa/VisitSDJPAService.java
@@ -0,0 +1,47 @@
+package local.springframework.sfgpetclinic.services.springdatajpa;
+
+import local.springframework.sfgpetclinic.model.Visit;
+import local.springframework.sfgpetclinic.repositories.VisitRepository;
+import local.springframework.sfgpetclinic.services.VisitService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+@Profile("springdatajpa")
+public class VisitSDJPAService implements VisitService {
+    private final VisitRepository visitRepository;
+
+    public VisitSDJPAService(VisitRepository visitRepository) {
+        this.visitRepository = visitRepository;
+    }
+
+    @Override
+    public Set<Visit> findAll() {
+        Set<Visit> visits = new HashSet<>();
+        visitRepository.findAll().forEach(visits::add);
+        return visits;
+    }
+
+    @Override
+    public Visit findById(Long id) {
+        return visitRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public Visit save(Visit object) {
+        return visitRepository.save(object);
+    }
+
+    @Override
+    public void delete(Visit object) {
+        visitRepository.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        visitRepository.deleteById(id);
+    }
+}
